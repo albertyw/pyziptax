702672bdc724df450c352ee322e5bd50e3a1361a
Albert Wang <albert@zerocater.com>
2014-08-31 18:15:00 -0700

    Added example script

---
+++ examples/example.py
@@ -0,0 +1,8 @@
+import pyziptax
+
+pyziptax.api_key = "ASDF1234"
+print pyziptax.get_rate('10001', 'New York', 'NY', False)
+# 8.875
+
+print pyziptax.get_rate('94304', multiple_rates=True)
+# {u'LOCKHEED': Decimal('8.250'), u'PALO ALTO': Decimal('8.750')}
